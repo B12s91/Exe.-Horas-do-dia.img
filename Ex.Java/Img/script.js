b2b3fcb31780bbb0f4419ead225ce062c459a20b
Bruno.JS <brunosantospro01@gmail.com>
2025-01-23 22:26:09 -0300

    Add files via upload
    Este projeto é um exercício do curso JavaScript do Gustavo Guanabara, focado no uso do DOM (Document Object Model) para criar uma página que exibe a hora atual de forma dinâmica e muda o conteúdo com base no horário.
    
    Funcionalidades
    Exibe a hora atual no formato "Agora são XX horas".
    Altera a imagem exibida conforme o horário (manhã, tarde ou noite).
    Personalização de cores para a página com base no período do dia.
    Tecnologias Utilizadas
    HTML: Estrutura da página.
    CSS: Estilização para personalizar o layout e as cores.
    JavaScript: Manipulação do DOM para exibir a hora e alterar dinamicamente o conteúdo.
    Como Executar
    Baixe ou clone o repositório.
    Abra o arquivo index.html (ou o nome correspondente) em qualquer navegador.
    Exemplo Visual
    Veja abaixo um exemplo de como a página se apresenta à noite:
    
    
    Créditos
    Exercício baseado no conteúdo do curso CursoemVideo - JavaScript.

---
+++ Ex.Java/Img/script.js
@@ -0,0 +1,17 @@
+function carregar ()  {
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = 2 //data.getHours()
+msg.innerHTML= `agora são ${hora} horas.`
+
+if (hora >= 0 && hora < 12) {
+  img.src = "fotoManha.jpg"
+
+} else if (hora >= 12 && hora <18){
+  img.src = "fotoTarde.jpg"
+
+} else {
+  img.src = "fotoNoite"
+} 
+}
